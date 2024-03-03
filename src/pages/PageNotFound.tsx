199c716ed92ca1fa02bcb61dcaf1e62145a77525
NicTheFifth <48408556+NicTheFifth@users.noreply.github.com>
2024-03-03 21:55:39 +0100

    page not found linking to CG

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return <Link to="componentgenerator">PageNotFound</Link>;
 };
 
 export default PageNotFound;
