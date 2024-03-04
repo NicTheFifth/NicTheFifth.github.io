4613d974be19801635aa93f2b1111662edcc47cd
NicTheFifth <48408556+NicTheFifth@users.noreply.github.com>
2024-03-04 02:15:55 +0100

    feat: chatcolour selector test

--- src/pages/ComponentGenerator.tsx
+++ src/pages/ComponentGenerator.tsx
@@ -1,5 +1,57 @@
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+
 const ComponentGenerator = () => {
-  return <button>ComponentGenerator</button>;
+  const ChatColors = [
+    "Aqua",
+    "Black",
+    "Blue",
+    "Dark_Aqua",
+    "Dark_Blue",
+    "Dark_Gray",
+    "Dark_Green",
+    "Dark_Purple",
+    "Dark_Red",
+    "Gold",
+    "Gray",
+    "Green",
+    "Light_Purple",
+    "Magic",
+    "Red",
+    "White",
+    "Yellow",
+  ] as const;
+  type ChatColor = (typeof ChatColors)[number];
+
+  type BaseComponent = {
+    value: string;
+    colour: ChatColor;
+  };
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="Chat_colour"
+        select
+        label="Select"
+        defaultValue="EUR"
+        helperText="Select chatcolour for component"
+      >
+        {ChatColors.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </TextField>
+    </Box>
+  );
 };
 
 export default ComponentGenerator;
