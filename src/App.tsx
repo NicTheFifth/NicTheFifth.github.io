a1adfb82d769482acec470d2f7df8e4542a12322
NicTheFifth <48408556+NicTheFifth@users.noreply.github.com>
2024-05-24 00:46:56 +0200

    feat:background

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createHashRouter, RouterProvider } from "react-router-dom";
 import ComponentGenerator from "./pages/ComponentGenerator";
 import PageNotFound from "./pages/PageNotFound";
 import HomePage from "./pages/HomePage";
+import Box from "@mui/material/Box";
 
 function App() {
   const router = createHashRouter([
@@ -19,7 +20,15 @@ function App() {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <Box
+      sx={{ background: "linear-gradient(#1a857d, #167c4c)" }}
+      minHeight={100}
+      minWidth={100}
+    >
+      <RouterProvider router={router} />
+    </Box>
+  );
 }
 
 export default App;
